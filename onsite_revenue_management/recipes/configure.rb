c3a485ecf4d56152831b1e5d272f65fcc99fbdc0
February <fskai@on-site.com>
2015-06-02 11:29:54 -0700

    Attempt to build secrets from OpsWorks environment variables

--- onsite_revenue_management/recipes/configure.rb
+++ onsite_revenue_management/recipes/configure.rb
@@ -6,4 +6,25 @@ node[:deploy].each do |application, deploy|
     content "Just a test: #{application} #{ENV['RX_API_SITE']}"
     user "deploy"
   end
+
+
+  secrets_file = File.join(deploy[:current_path], 'config', 'secrets.yml')
+  template "#{deploy[:deploy_to]}/shared/config/secrets.yml" do
+    mode '0644'
+    owner deploy[:user]
+    group deploy[:group]
+    source 'secrets.yml.erb'
+    variables(
+      environment:
+        OpsWorks::Escape.escape_double_quotes(deploy[:environment_variables])
+    )
+
+    notifies :run, "execute[restart Rails app #{application}]"
+
+    only_if do
+      deploy[:database][:host].present? &&
+        File.directory?("#{deploy[:deploy_to]}/shared/config/")
+    end
+  end
+
 end
