486e341639f37b7cee3c47df80fb052e5a7ee702
February <fskai@on-site.com>
2015-06-02 09:30:12 -0700

    Experiment with OpsWorks node attributes

--- onsite_revenue_management/recipes/configure.rb
+++ onsite_revenue_management/recipes/configure.rb
@@ -1,4 +1,9 @@
-file "/home/deploy/fhk_test" do
-  content "Just a test"
-  user "deploy"
+
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+  test_file = File.join(deploy[:current_path], 'config', 'fhk_test')
+  file test_file do
+    content "Just a test: #{application} #{ENV['RX_API_SITE']}"
+    user "deploy"
+  end
 end
