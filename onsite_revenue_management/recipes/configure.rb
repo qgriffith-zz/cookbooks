54de686cd7ebfa5758b608150ed5408c53ec4a94
February <fskai@on-site.com>
2015-06-02 12:01:40 -0700

    Determining default values of node symlink attribute

--- onsite_revenue_management/recipes/configure.rb
+++ onsite_revenue_management/recipes/configure.rb
@@ -3,7 +3,7 @@ node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
   test_file = File.join(deploy[:current_path], 'config', 'fhk_test')
   file test_file do
-    content "Just a test: #{application} #{ENV['RX_API_SITE']}"
+    content deploy[:symlink_before_migrate]
     user "deploy"
   end
 
