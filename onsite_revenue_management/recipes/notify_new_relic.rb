3627ac41fa14c930c62eb4c5c9e5e83e5bd1c125
February <fskai@on-site.com>
2015-08-13 12:03:47 -0700

    Fix a typo

--- onsite_revenue_management/recipes/notify_new_relic.rb
+++ onsite_revenue_management/recipes/notify_new_relic.rb
@@ -5,7 +5,7 @@ node[:deploy].each do |application, deploy|
     command "bundle exec newrelic deployments -r `git log --pretty=format:'%h' -n 1`"
     user deploy[:user]
     group deploy[:group]
-    pwd "#{deploy[:deploy_to]}/current"
+    cwd "#{deploy[:deploy_to]}/current"
     environment({'RAILS_ENV' => deploy[:rails_env]})
   end
 
