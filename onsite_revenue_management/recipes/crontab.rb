25ace0304d803425749d1343818eb415bc7a6a27
February <fskai@on-site.com>
2015-08-13 08:57:23 -0700

    Add a crontab installer

---
+++ onsite_revenue_management/recipes/crontab.rb
@@ -0,0 +1,19 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  crontab_path = File.join(deploy[:deploy_to], 'config', 'crontab')
+
+  execute "install #{application} crontab" do
+    command "crontab -u #{deploy[:user]} #{crontab_path}"
+    not_if do
+      if ! File.exists?(crontab_path)
+        true
+      else
+        current = `crontab -u #{deploy[:user]}`
+        desired = File.read(crontab_path)
+        current == desired
+      end
+    end
+  end
+
+end
