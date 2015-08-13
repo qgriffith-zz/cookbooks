ce47f5a4eb53772ea5f8cc6700ccd3388f735a39
February <fskai@on-site.com>
2015-08-13 11:17:52 -0700

    Add recipe to notify new relic of our deployments

---
+++ onsite_revenue_management/recipes/notify_new_relic.rb
@@ -0,0 +1,14 @@
+# Get the Revision `git log --pretty=format:'%h' -n 1`
+# bundle exec newrelic deployment
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "Notify New Relic" do
+    command "bundle exec newrelic deployments -r `git log --pretty=format:'%h' -n 1`"
+    user deploy[:user]
+    group deploy[:group]
+    pwd "#{deploy[:deploy_to]}/current"
+    environment { RAILS_ENV: deploy[:rails_env] }
+  end
+
+end
