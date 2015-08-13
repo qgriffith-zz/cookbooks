c7e45f98999bf17ce19e5fe2af1b435815d65062
February <fskai@on-site.com>
2015-08-13 11:27:38 -0700

    Correct some errors in notify_new_relic

--- onsite_revenue_management/recipes/notify_new_relic.rb
+++ onsite_revenue_management/recipes/notify_new_relic.rb
@@ -6,7 +6,7 @@ node[:deploy].each do |application, deploy|
     user deploy[:user]
     group deploy[:group]
     pwd "#{deploy[:deploy_to]}/current"
-    environment { 'RAILS_ENV' => deploy[:rails_env] }
+    environment({'RAILS_ENV' => deploy[:rails_env]})
   end
 
 end
