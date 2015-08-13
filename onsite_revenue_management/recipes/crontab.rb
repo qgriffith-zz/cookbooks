001e21005b631a7e132ef2fed5e70a51058e44b6
February <fskai@on-site.com>
2015-08-13 09:56:04 -0700

    Correct path from crontab

--- onsite_revenue_management/recipes/crontab.rb
+++ onsite_revenue_management/recipes/crontab.rb
@@ -1,9 +1,9 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
 
-  crontab_path = File.join(deploy[:deploy_to], 'config', 'crontab')
+  crontab_path = File.join(deploy[:deploy_to], 'current', 'config', 'crontab')
 
-  execute "install #{application} crontab #{crontab_path}" do
+  execute "crontab #{crontab_path}" do
     command "crontab -u #{deploy[:user]} #{crontab_path}"
     not_if do
       if ! File.exists?(crontab_path)
