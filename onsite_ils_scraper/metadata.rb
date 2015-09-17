ddb9dbc2ab36c08985c5d9e1caf702cf34bdd630
February <fskai@on-site.com>
2015-09-17 10:12:14 -0700

    Add a crontab recipe for the ILS Scraper

---
+++ onsite_ils_scraper/metadata.rb
@@ -0,0 +1,5 @@
+name "onsite_ils_scraper"
+description "Configures the ILS Scraper"
+maintainer "On-Site.com"
+license "(c) 2015 All Rights Reserved"
+version "0.1.0"
---
+++ onsite_ils_scraper/recipes/crontab.rb
@@ -0,0 +1,33 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  app_path = File.join(deploy[:deploy_to], 'current')
+  crontab_path = File.join(deploy[:home], 'crontab')
+
+  template crontab_path do
+    source "#{application}.crontab.erb"
+    mode '0644'
+    owner deploy[:user]
+    group deploy[:group]
+    variables(
+      scredule: deploy[:environment_variables]['ILS_SCHEDULE'],
+      environment: deploy[:environment_variables]['ILS_ENV'],
+      app_path: rails_path,
+      sites: deploy[:environment_variables]['ILS_SITES'],
+    )
+  end
+
+  execute "crontab #{crontab_path}" do
+    command "crontab -u #{deploy[:user]} #{crontab_path}"
+    not_if do
+      if ! File.exists?(crontab_path)
+        true
+      else
+        current = `crontab -u #{deploy[:user]}`
+        desired = File.read(crontab_path)
+        current == desired
+      end
+    end
+  end
+
+end
