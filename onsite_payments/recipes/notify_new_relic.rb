1a85de2e23be1e7c50c253d2dfb06882075ee7e3
Ben Ellis <bellis@on-site.com>
2015-09-23 03:24:00 -0700

    Add 1st draft of onsite_payments recipes The crontab entries are a rough draft. TODO: * add a 3rd entry once there's a rake task for it. * confirm the time of day for the cron entries (current values are   not confirmed) * consider whether we need some way to monitor the cron

---
+++ onsite_payments/recipes/notify_new_relic.rb
@@ -0,0 +1,12 @@
+node[:deploy].each do |application, deploy|
+  deploy = node[:deploy][application]
+
+  execute "Notify New Relic" do
+    command "bundle exec newrelic deployments -r `git log --pretty=format:'%h' -n 1`"
+    user deploy[:user]
+    group deploy[:group]
+    cwd "#{deploy[:deploy_to]}/current"
+    environment({'RAILS_ENV' => deploy[:rails_env]})
+  end
+
+end
