7d8f0003c0ab0a0c209e053cdd35c08506f407cd
Eric Knecht <eknecht@on-site.com>
2016-07-19 17:31:03 -0400

    Opsworks chef does not support issues_url

--- opsworks_datadog/metadata.rb
+++ opsworks_datadog/metadata.rb
@@ -5,8 +5,6 @@ license 'all_rights'
 description 'Installs/Configures opsworks_datadog'
 long_description 'Installs/Configures opsworks_datadog'
 
-issues_url "https://github.on-site.com/on-site/#{name}/issues"
-source_url "https://github.on-site.com/on-site/#{name}"
 
 depends 'datadog'
 version '0.1.0'
