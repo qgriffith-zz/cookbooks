1314211fb2f627dad66da125c33bf6b36046725d
Eric Knecht <eknecht@on-site.com>
2016-07-20 19:19:50 -0400

    Need the region, maybe?

--- opsworks_jira_stats/recipes/default.rb
+++ opsworks_jira_stats/recipes/default.rb
@@ -3,5 +3,6 @@ include_recipe 's3_file'
 s3_file "#{node["deploy"]["jira_stats"]["deploy_to"]}/jira_private_key.pem" do
   bucket 'osm-opsworks-secrets'
   remote_path 'jira_private_key.pem'
+  s3_url "https://s3-us-west-2.amazonaws.com/osm-opsworks-secrets"
   mode "0644"
 end
