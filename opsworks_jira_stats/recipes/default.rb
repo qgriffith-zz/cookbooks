1ff3825ad96f84159d0026dbecf853eb87adf87a
Eric Knecht <eknecht@on-site.com>
2016-07-27 12:45:04 -0400

    attempt to load aws key via environment variables

--- opsworks_jira_stats/recipes/default.rb
+++ opsworks_jira_stats/recipes/default.rb
@@ -1,8 +1,16 @@
 include_recipe 's3_file'
 
-s3_file "#{node["deploy"]["jira_stats"]["deploy_to"]}/jira_private_key.pem" do
-  bucket 'osm-opsworks-secrets'
-  remote_path 'jira_private_key.pem'
-  s3_url "https://s3-us-west-2.amazonaws.com/osm-opsworks-secrets"
-  mode "0644"
+
+
+node[:deploy].each do |application, deploy|
+
+  s3_file "#{node["deploy"]["jira_stats"]["deploy_to"]}/jira_private_key.pem" do
+    bucket 'osm-opsworks-secrets'
+    remote_path 'jira_private_key.pem'
+    s3_url "https://s3-us-west-2.amazonaws.com/osm-opsworks-secrets"
+    mode "0644"
+    aws_access_key_id     deploy[:environment_variables]['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key deploy[:environment_variables]['AWS_SECRET_ACCESS_KEY']
+  end
+
 end
