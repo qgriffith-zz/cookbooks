08edc9587843b0f40701a666c8d6427cf96dca14
Eric Knecht <eknecht@on-site.com>
2016-11-01 11:28:08 -0400

    fix chef error

--- onsite_revenue_management/recipes/default.rb
+++ onsite_revenue_management/recipes/default.rb
@@ -8,13 +8,14 @@ node[:deploy].each do |application, deploy|
     mode  '0755'
     source 'environment.sh.erb'
   end
+
+  template "/etc/init/sidekiq.conf" do
+    source "sidekiq.conf.erb"
+    variables(
+      redis_server: deploy[:environment_variables]['REDIS_HOST'],
+      app_path: app_path,
+      user: deploy[:user]
+    )
+  end
 end
 
-template "/etc/init/sidekiq.conf" do
-  source "sidekiq.conf.erb"
-  variables(
-    redis_server: deploy[:environment_variables]['REDIS_HOST'],
-    app_path: app_path,
-    user: deploy[:user]
-  )
-end
