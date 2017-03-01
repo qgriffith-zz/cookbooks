57257426bd738966d4e8631799c2b1d194748d68
Isaac Betesh <ibetesh@on-site.com>
2017-03-01 14:20:53 -0500

    Add SECRET_KEY_BASE to env during precompile

--- opsworks_yakmail/recipes/precompile.rb
+++ opsworks_yakmail/recipes/precompile.rb
@@ -1,6 +1,7 @@
 node[:deploy].each do |application, deploy|
   rails_env = deploy[:rails_env]
   current_path = deploy[:current_path]
+  secret_key_base = deploy[:environment_variables]['SECRET_KEY_BASE']
 
   Chef::Log.info("Precompiling Rails assets with environment #{rails_env}")
 
@@ -8,6 +9,6 @@ node[:deploy].each do |application, deploy|
     cwd current_path
     user 'deploy'
     command 'bundle exec rake assets:precompile'
-    environment 'RAILS_ENV' => rails_env
+    environment 'RAILS_ENV' => rails_env, 'SECRET_KEY_BASE' => secret_key_base
   end
 end
